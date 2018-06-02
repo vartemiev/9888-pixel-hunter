d1b453f73d3c71c3186a7a4c8759588250313133
vartemiev <vartemiev@xiag.ch>
2018-06-02 19:29:16 +0700

    Updated element creation functions, add elements composer

--- js/create-element.js
+++ js/create-element.js
@@ -1,6 +1,19 @@
-export const createElement = (template) => {
-  const container = document.createElement(`span`);
+export const createElement = (template, options = {}) => {
+  const {elem = `div`, classList = []} = options;
+
+  const container = document.createElement(elem);
+  container.classList.add(...classList);
   container.innerHTML = template;
 
   return container;
+};
+
+export const composeElements = (elements) => {
+  const fragment = document.createDocumentFragment();
+
+  for (let el of elements) {
+    fragment.appendChild(el);
+  }
+
+  return fragment;
 };
